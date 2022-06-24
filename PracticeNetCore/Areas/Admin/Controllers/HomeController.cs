4da6485117f36376b64458f381a2a61b6017b067
Mekan Hydyrov <mekanhydyrov@gmail.com>
2022-06-25 02:16:53 +0300

    Ürün Listele

--- PracticeNetCore/Areas/Admin/Controllers/HomeController.cs
+++ PracticeNetCore/Areas/Admin/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PracticeNetCore.Interfaces;
 
 namespace PracticeNetCore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize]
     public class HomeController : Controller
     {
-        [Area("Admin")]
-        [Authorize]
+        private readonly IUrunRepository _urunRepository;
+        public HomeController(IUrunRepository urunRepository)
+        {
+            _urunRepository = urunRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_urunRepository.GetirHepsi());
         }
     }
 }
